3fd456694139ee73daa14c439d96b9471a06cc77
Kcraft059 <kcraft059.msg@gmail.com>
2026-01-07 21:57:42 +0100

    ;(

--- testmem.c
+++ testmem.c
@@ -5,7 +5,8 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#define IO_BANK0_ADDR 0x400d0000
+#define IO_BANK0_PHYS 0x400d0000
+#define IO_BANK0_SIZE 0x128
 
 int main() {
   int fd = open("/dev/mem", O_RDWR | O_SYNC);
@@ -14,17 +15,18 @@ int main() {
     return 1;
   }
 
-  // map 4KB (enough for IO_BANK0)
-  volatile uint32_t* gpio_base = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, IO_BANK0_ADDR);
+  // Can't be done since, mem remap & access is forbidden to anything other than kernel drivers
+  volatile uint32_t* gpio_base = mmap(NULL, IO_BANK0_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, IO_BANK0_PHYS);
+
   if (gpio_base == MAP_FAILED) {
     perror("mmap");
     return 1;
   }
 
-  volatile int64_t* gpio_pin2 = (void*)gpio_base + 2 * 8;
+  volatile int64_t* gpio_pin2 = (void*)gpio_base + 2 * 0x8;
 
-  volatile int32_t* gpio2_ctrl = (void*)gpio_pin2;
-  volatile int32_t* gpio2_status = (void*)gpio_pin2 + 4;
+  volatile int32_t* gpio2_status = (void*)gpio_pin2;
+  volatile int32_t* gpio2_ctrl = (void*)gpio_pin2 + 0x4;
 
   printf("GPIO base: %p\n\
     \tGPIO2 Cluster: %p\n\
