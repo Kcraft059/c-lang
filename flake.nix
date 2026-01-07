38188598368c9151427dc426daf9c899a13b6233
Kcraft059 <kcraft059.msg@gmail.com>
2026-01-07 17:17:16 +0100

    fixed EOF

--- flake.nix
+++ flake.nix
@@ -15,9 +15,9 @@
     {
       devShells."${system}".default = pkgs.mkShell {
         packages = with pkgs; [
-          libgpiod 	# Linux GPIO lib
-          lgpio		# Simplified GPIO lib
-	  pigpio 	# Pi-specific GPIO control
+          libgpiod # Linux GPIO lib
+          lgpio # Simplified GPIO lib
+          pigpio # Pi-specific GPIO control
           gnumake
           bear
           clang
@@ -29,9 +29,8 @@
             Add:
               - -I${pkgs.glibc.dev}/include
               - -I${pkgs.lgpio}/include
-	      - -I${pkgs.pigpio}/include
-
-	  EOF
+              - -I${pkgs.pigpio}/include
+          EOF
 
           exec ${pkgs.zsh}/bin/zsh
         '';
